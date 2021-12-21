1c7eadc0cabd14a3cc3dbf857af485db25263924
dimaswntech <67653363+dimaswntech@users.noreply.github.com>
2021-12-21 16:12:24 +0700

    Add files via upload

---
+++ tugas21.js
@@ -0,0 +1,10 @@
+// Tugas 21 Javascript
+
+// Buatlah perubahan kata “masak” menjadi “makan” dalam kalimat “Saya belajar masak nasi goreng” sehingga akan tampil seperti gambar berikut ini :
+
+function tugas(){
+    var x = "Saya belajar masak nasi goreng";
+    console.log(x);
+    console.log(x.replace("masak","makan"));
+}
+tugas();
